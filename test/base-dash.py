f61098efe9bb3be0d3ef51263a621f6727c7390f
spoxus <ming.mge@gmail.com>
2023-05-20 21:30:45 -0700

    temperary stage for testing dash

--- test/base-dash.py
+++ test/base-dash.py
@@ -3,34 +3,36 @@ import random
 
 app = Dash(__name__)
 
-figure = dict(
-    data = [
-        {'x': [0],
+data = [{'x': [0],
          'y': [0],
-         'mode':'lines+markers'
+         'name': 'bid',
+         'mode': 'lines+markers'
         },
         {'x': [0],
          'y': [0],
-         'mode':'lines'
+         'name': 'ask',
+         'mode': 'lines'
         },
         {'x': [0],
          'y': [0],
-         'mode':'markers'
+         'name': 'SMA1',
+         'mode': 'markers'
         }
-    ]
-)
+]
+
+figure = dict(data = data)
 
 app.layout = html.Div([
     html.Div([
         html.H3('Extend multiple traces at once'),
         dcc.Graph(
-            id = 'graph',
+            id     = 'graph',
             figure = figure
         )
     ]),
     dcc.Interval(
-        id = 'interval',
-        interval = (3 * 1000),
+        id          = 'interval',
+        interval    = (5 * 1000),
         n_intervals = 0
     )
 ])
@@ -41,17 +43,30 @@ app.layout = html.Div([
     Input ('interval', 'n_intervals'),
     State ('graph',    'figure')
 )
+
 def update_traces(n_intervals, existing):
+    # I want to find out what x and y do
+    # x: move right, 1, 2, 3, 4, 5...
+    # y: the value associated with x
     x = [[existing['data'][0]['x'][-1] + 1],
-         [existing['data'][1]['x'][-1] - .5, existing['data'][1]['x'][-1] + 1],
+         [existing['data'][1]['x'][-1] - .5,
+          existing['data'][1]['x'][-1] + 1
+         ],
          [existing['data'][2]['x'][-1] + .1]
         ]
-    y = [[random.random()],
+    y = [[random.random() * 3], # this is trace 'bid'
          [0, random.random()],
          [random.random()]
         ]
+    # 
     return (dict(x=x, y=y),
-            [0, 1, 2]
+            # what does this list do???
+            # if i change anything here, won't work
+            [0,
+             1,
+             2,
+             #3
+            ]
            )
 
 
--- test/try7.py
+++ test/try7.py
@@ -3,28 +3,24 @@ import random
 
 app = Dash(__name__)
 
-figure = dict(
-    data = [
-        # trace 1
-        {'x': [0],
+data = [{'x': [0],
          'y': [0],
          'name': 'bid',
          'mode': 'lines+markers'
         },
-        # trace 2
         {'x': [0],
          'y': [0],
          'name': 'ask',
          'mode': 'lines'
         },
-        # trace 3
         {'x': [0],
          'y': [0],
          'name': 'SMA1',
          'mode': 'markers'
         }
-    ]
-)
+]
+
+figure = dict(data = data)
 
 app.layout = html.Div([
     html.Div([
@@ -36,7 +32,7 @@ app.layout = html.Div([
     ]),
     dcc.Interval(
         id          = 'interval',
-        interval    = (3 * 1000),
+        interval    = (5 * 1000),
         n_intervals = 0
     )
 ])
@@ -50,19 +46,22 @@ app.layout = html.Div([
 
 def update_traces(n_intervals, existing):
     # I want to find out what x and y do
+    # x: move right, 1, 2, 3, 4, 5...
+    # y: the value associated with x
     x = [[existing['data'][0]['x'][-1] + 1],
          [existing['data'][1]['x'][-1] - .5,
           existing['data'][1]['x'][-1] + 1
          ],
          [existing['data'][2]['x'][-1] + .1]
         ]
-    y = [[random.random()],
+    y = [[random.random() * 3], # this is trace 'bid'
          [0, random.random()],
          [random.random()]
         ]
     # 
     return (dict(x=x, y=y),
             # what does this list do???
+            # if i change anything here, won't work
             [0,
              1,
              2,
