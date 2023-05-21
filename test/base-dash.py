2d84628bbb453cc21518f2b3a58cf9e49553c89f
spoxus <ming.mge@gmail.com>
2023-05-20 20:24:10 -0700

    new file, testing out the dash data structure

--- test/base-dash.py
+++ test/base-dash.py
@@ -1,89 +1,58 @@
-import dash
-from dash.dependencies import Input, Output, State
-from dash import html, dcc
+from   dash import html, dcc, Input, Output, State, Dash
 import random
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 
+figure = dict(
+    data = [
+        {'x': [0],
+         'y': [0],
+         'mode':'lines+markers'
+        },
+        {'x': [0],
+         'y': [0],
+         'mode':'lines'
+        },
+        {'x': [0],
+         'y': [0],
+         'mode':'markers'
+        }
+    ]
+)
 
 app.layout = html.Div([
     html.Div([
-        html.H3('Extend a specific trace'),
-        dcc.Dropdown(
-            id='trace-selection',
-            options=[
-                {'label': 'extend trace 0', 'value': 0},
-                {'label': 'extend trace 1', 'value': 1},
-            ],
-            value=0
-        ),
-        dcc.Graph(
-            id='graph-extendable',
-            figure=dict(
-                data=[{'x': [0, 1, 2, 3, 4],
-                       'y': [0, .5, 1, .5, 0],
-                       'mode':'lines+markers'
-                       },
-                      {'x': [0, 1, 2, 3, 4],
-                       'y': [1, 1, 1, 1, 1],
-                       'mode':'lines+markers'
-                       }],
-            )
-        ),
-    ]),
-    html.Div([
         html.H3('Extend multiple traces at once'),
         dcc.Graph(
-            id='graph-extendable-2',
-            figure=dict(
-                data=[{'x': [0, 1, 2, 3, 4],
-                       'y': [0, .5, 1, .5, 0],
-                       'mode':'lines+markers'
-                       },
-                      {'x': [0],
-                       'y': [0],
-                       'mode':'lines'
-                       },
-                      {'x': [0, .1, .2, .3, .4],
-                       'y': [0, 0, 0, 0, 0],
-                       'mode':'markers'
-                       }],
-            )
-        ),
+            id = 'graph',
+            figure = figure
+        )
     ]),
     dcc.Interval(
-        id='interval-graph-update',
-        interval=1000,
-        n_intervals=0),
+        id = 'interval',
+        interval = (3 * 1000),
+        n_intervals = 0
+    )
 ])
 
 
-@app.callback(Output('graph-extendable', 'extendData'),
-              [Input('interval-graph-update', 'n_intervals')],
-              [State('graph-extendable', 'figure'),
-               State('trace-selection', 'value')])
-def update_extend_traces_traceselect(n_intervals, existing, trace_selection):
-    x_new = existing['data'][trace_selection]['x'][-1] + 1
-    y_new = random.random()
-    return dict(x=[[x_new]], y=[[y_new]]), [trace_selection]
-
-
-@app.callback(Output('graph-extendable-2', 'extendData'),
-              [Input('interval-graph-update', 'n_intervals')],
-              [State('graph-extendable-2', 'figure')])
-def update_extend_traces_simult(n_intervals, existing):
-    return (dict(x=[
-        [existing['data'][0]['x'][-1] + 1],
-        [existing['data'][1]['x'][-1] - .5, existing['data'][1]['x'][-1] + 1],
-        [existing['data'][2]['x'][-1] + .1]
-    ],
-        y=[
-        [random.random()],
-        [0, random.random()],
-        [random.random()]
-    ]),
-        [0, 1, 2]
-    )
+@app.callback(
+    Output('graph',    'extendData'),
+    Input ('interval', 'n_intervals'),
+    State ('graph',    'figure')
+)
+def update_traces(n_intervals, existing):
+    x = [[existing['data'][0]['x'][-1] + 1],
+         [existing['data'][1]['x'][-1] - .5, existing['data'][1]['x'][-1] + 1],
+         [existing['data'][2]['x'][-1] + .1]
+        ]
+    y = [[random.random()],
+         [0, random.random()],
+         [random.random()]
+        ]
+    return (dict(x=x, y=y),
+            [0, 1, 2]
+           )
 
 
 if __name__ == '__main__':
--- test/try7.py
+++ test/try7.py
@@ -1,53 +1,73 @@
-import dash
-from   dash import html, dcc, Input, Output, State
+from   dash import html, dcc, Input, Output, State, Dash
 import random
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 
+figure = dict(
+    data = [
+        # trace 1
+        {'x': [0],
+         'y': [0],
+         'name': 'bid',
+         'mode': 'lines+markers'
+        },
+        # trace 2
+        {'x': [0],
+         'y': [0],
+         'name': 'ask',
+         'mode': 'lines'
+        },
+        # trace 3
+        {'x': [0],
+         'y': [0],
+         'name': 'SMA1',
+         'mode': 'markers'
+        }
+    ]
+)
 
 app.layout = html.Div([
     html.Div([
         html.H3('Extend multiple traces at once'),
         dcc.Graph(
-            id='graph',
-            figure=dict(
-                data=[{'x': [0],
-                       'y': [0],
-                       'mode':'lines+markers'
-                       },
-                      {'x': [0],
-                       'y': [0],
-                       'mode':'lines'
-                       },
-                      {'x': [0],
-                       'y': [0],
-                       'mode':'markers'
-                       }],
-            )
-        ),
+            id     = 'graph',
+            figure = figure
+        )
     ]),
     dcc.Interval(
-        id='interval',
-        interval = (3 * 1000),
-        n_intervals = 0),
+        id          = 'interval',
+        interval    = (3 * 1000),
+        n_intervals = 0
+    )
 ])
 
 
-@app.callback(Output('graph',    'extendData'),
-              Input ('interval', 'n_intervals'),
-              State ('graph',    'figure')
-             )
+@app.callback(
+    Output('graph',    'extendData'),
+    Input ('interval', 'n_intervals'),
+    State ('graph',    'figure')
+)
+
 def update_traces(n_intervals, existing):
+    # I want to find out what x and y do
     x = [[existing['data'][0]['x'][-1] + 1],
-         [existing['data'][1]['x'][-1] - .5, existing['data'][1]['x'][-1] + 1],
+         [existing['data'][1]['x'][-1] - .5,
+          existing['data'][1]['x'][-1] + 1
+         ],
          [existing['data'][2]['x'][-1] + .1]
         ]
     y = [[random.random()],
          [0, random.random()],
          [random.random()]
         ]
+    # 
     return (dict(x=x, y=y),
-            [0, 1, 2]
+            # what does this list do???
+            [0,
+             1,
+             2,
+             #3
+            ]
            )
 
 
