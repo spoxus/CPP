be4e71f93290606042ad9ba66e3ccb6f18b2667b
spoxus <ming.mge@gmail.com>
2023-05-31 10:59:47 -0700

    fleet of aliens

--- ai/ailen_invasion.py
+++ ai/ailen_invasion.py
@@ -22,20 +22,24 @@ class AilenInvasion:
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
         
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
         
     def _create_fleet(self):
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            current_x = alien_width
+            current_y += 2 * alien_height
         #self.aliens.add(alien)
 
     def run_game(self):
